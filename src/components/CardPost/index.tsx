3cbcf750e2185dca1e671e8f23d54a7875b91735
evelynhelena <evelynhelena781@gmail.com>
2022-01-25 19:02:24 -0300

    retirando comentário

--- src/components/CardPost/index.tsx
+++ src/components/CardPost/index.tsx
@@ -1,17 +1,3 @@
-/*import styles from "./styles.module.scss";
-export function CardPost() {
-    return (
-        <div className={styles.sectionContainer}>
-            <img src="/images/html-css.jpg" alt="html e css" />
-            <div className={styles.content}>
-                <p>HtML e CSS</p>
-                <h2>Fala Dev, Nesse post vamos falar um pouco sobre HTML e CSS</h2>
-                <span>By: Evelyn Helena</span>
-            </div>
-        </div>
-    );
-}*/
-
 import * as React from 'react';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
